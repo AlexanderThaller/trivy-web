36406847abeea2bc110c8b9f7af9ba9710bee323
Alexander Thaller <alexander@thaller.ws>
2024-07-24 10:20:14 +0200

    Cleanup

---
+++ src/signal.rs
@@ -0,0 +1,35 @@
+use tokio::signal;
+use tracing::{
+    event,
+    Level,
+};
+
+pub(super) async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    let signal = tokio::select! {
+        () = ctrl_c => {
+            "SIGINT (CTRL+C)"
+        },
+        () = terminate => {
+            "SIGTERM"
+        },
+    };
+
+    event!(
+        Level::INFO,
+        signal = signal,
+        "Signal received, shutting down"
+    );
+}
