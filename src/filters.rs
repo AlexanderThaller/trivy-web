5e3433b33161e9414ed53935f103cc45fda10711
Alexander Thaller <alexander@thaller.ws>
2025-10-24 10:59:21 +0200

    Update askama from 0.12 to 0.14

--- src/filters.rs
+++ src/filters.rs
@@ -2,7 +2,10 @@
     clippy::unnecessary_wraps,
     reason = "can not change this as this is what the askama crate expects"
 )]
-pub fn ansi_to_html<T: std::fmt::Display>(s: T) -> ::askama::Result<String> {
+pub fn ansi_to_html<T: std::fmt::Display>(
+    s: T,
+    _: &dyn askama::Values,
+) -> ::askama::Result<String> {
     let s = s.to_string();
     Ok(s.replace('\n', "<br />"))
 }
@@ -11,7 +14,7 @@ pub fn ansi_to_html<T: std::fmt::Display>(s: T) -> ::askama::Result<String> {
     clippy::unnecessary_wraps,
     reason = "can not change this as this is what the askama crate expects"
 )]
-pub fn format_error(err: &eyre::Error) -> ::askama::Result<String> {
+pub fn format_error(err: &eyre::Error, _: &dyn askama::Values) -> ::askama::Result<String> {
     let s = format!("{err:?}");
     Ok(s)
 }
